5358c9b7ab2a7f688f8732929366ac4a58be6b2e
Jay Burgess <jayburgess.p3@gmail.com>
2026-05-24 16:37:04 -0700

    feat: add barrel export for home components

---
+++ src/components/home/index.ts
@@ -0,0 +1,5 @@
+export { HeroBar } from "./HeroBar";
+export { InvestPanel } from "./InvestPanel";
+export { HouseholdPanel } from "./HouseholdPanel";
+export { WealthPanel } from "./WealthPanel";
+export { QuickActions } from "./QuickActions";
